694a590ecaec160634a671aef0c90e235d99ad7f
Caio-4lberto <caioalbertodasilva@gmail.com>
2022-05-24 10:31:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine','ejs'); //INFORMANDO PARA O EXPRESS USAR O EJS COMO VIEW ENGINE
+app.use(express.static('public'));  //UTILIZANDO DO EJS PARA UTILIZAR DE ARQUIVOS ESTÁTICOS (CSS, IMAGENS)
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/ask", (req, res) => {
+    res.render("ask");
+});
+
+
+
+app.listen(3000, () => {
+    console.log("List on the port http://localhost:3000/");
+});
+
+
+// AULA 31
